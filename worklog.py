9fb903b39cb8b6b6f9c6a98ec443d223b769db0d
Joe-mcgee <thulsmans.2994@me.com>
2017-03-14 05:54:18 -0600

    beta

--- worklog.py
+++ worklog.py
@@ -4,21 +4,23 @@
 # find by date, time spend, exact search, by pattern 
 import csv
 import os.path
-from ctypes.test.test_array_in_pointer import Value
+import datetime
+import re
+
 MENU_OPTIONS = ('(A)dd Entry', '(S)earch Entries', '(Q)uit')
 MENU_CHOICES = ('A', 'S', 'Q')
 VALIDATOR = ('Y', 'N') 
 entry_counter = 0
-
-
+SEARCH_OPTIONS = ('by (D)ate', 'by (T)ime Spent', 'by (E)xact Search', 'by (P)attern')
+SEARCH_CHOICES = ('D', 'T', 'E', 'P')
 
 class Entry:
-    def __init__(self, name, task_name, time, note):
+    def __init__(self, name, task_name, time, note, date):
         self.name = name
         self.task_name = task_name
         self.time = time
         self.note = note
-        
+        self.date = date
 
 def add_entry():
     print('Entry logger:')
@@ -100,23 +102,186 @@ def add_entry():
             print('what is the note you would like to add?')
         else:
             break
+        
+    while True:
+        try:
+            date_check = input('Was this work done Today? (Y/N)?:')
+            date_check.upper()
+            if date_check not in VALIDATOR:
+                raise ValueError
+            
+        except (TypeError, ValueError):
+            print('please select a valid date')
+        
+        else:
+            break
     
-    new_note = Entry(name, task_name, time, note)
+    if date_check == 'Y':
+        date = datetime.datetime.today().strftime("%Y/%m/%d")
+                
+    if date_check == 'N':
+        while True:
+            try:
+                year = int(input('What year was this work performed?: '))
+                year_check = input("is '{}' correct?: (Y/N): ".format(year))
+                if year_check not in VALIDATOR:
+                    raise ValueError
+            except (ValueError, TypeError):
+                print('please type in the correct year')
+                
+            else:
+                break
+            
+        while True:
+            try:
+                month = int(input('What month was this work performed? (as number): '))
+                month_check = input("is '{}' the correct month?: (Y/N) ".format(month))
+                if month_check not in VALIDATOR:
+                    raise ValueError
+            except (ValueError, TypeError):
+                print('please input the correct Month')
+                
+            else:
+                break
+        
+        while True:
+            try:
+                day = int(input('What day was this work performed? (as number): '))
+                day_check = input("is '{}' the correct day?: (Y/N) ".format(day))
+                if day_check not in VALIDATOR:
+                    raise ValueError
+            except (ValueError, TypeError):
+                print('please input the correct Day')
+                
+            else:
+                break
+                    
+        str_date= '{}-{}-{}'.format(year, month, day)
+        my_date = datetime.date(*[int(i) for i in str_date.split('-')])
+        date = my_date.strftime('%Y/%m/%d')
+    
+    new_note = Entry(name, task_name, time, note, date)
     
     with open("Work_log.csv", "a") as csvfile:
-        fieldnames = ['Name', 'Task Name', 'Time Spent', 'Note']
+        fieldnames = ['Name', 'Task Name', 'Time Spent', 'Note', 'Date']
         notewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
         notewriter.writerow({
             'Name': new_note.name,
             'Task Name': new_note.task_name,
             'Time Spent': new_note.time,
             'Note': new_note.note,
+            'Date': new_note.date,
             
             })
         
+    print('Note added succesfully!')
+    menu()
 
 def search_entries():
-    pass
+    print('How would you like to search?')
+    for option in SEARCH_OPTIONS:
+        print('> {}'.format(option))
+    while True:
+        try:
+            choice = input('Please select search type: ')
+            choice.upper()
+            if choice not in SEARCH_CHOICES:
+                raise ValueError
+        except (ValueError, TypeError):
+            print('please select a valid option')
+            
+        else:
+            break
+             
+    if choice == 'D':
+        while True:
+            try:
+                print('What date are you searching for?')
+                date = input('type in as YYYY/MM/DD: ')
+                date_list = date.split('/')
+                year= int(date_list[0])
+                month = int(date_list[1])
+                day = int(date_list[2])
+                date_check = datetime.datetime(year=year, month=month, day=day)
+                
+            
+            except (ValueError, TypeError):
+                print('Please select a valid date in form YYYY/MM/DD')
+                
+            else:
+                break
+        
+        with open('Work_log.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['Date'] == date:
+                    print(row)
+                
+    if choice == 'T':
+        while True:
+            try:
+                print('How long was the time spent on the log your looking for?')
+                time = int(input('Type a time in minutes: '))
+                
+            except (TypeError, ValueError):
+                print('please select a valid time in minutes')
+            
+            else:
+                break
+            
+        with open('Work_log.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if row['Time Spent'] == time:
+                    print(row)
+                    
+    if choice == 'E':
+        while True:
+            try:
+                print('Type in a search string')
+                search = input('what would you like to search?')
+                validator = input('is {} what you would like to search for? (Y/N): '.format(search))
+                if validator not in VALIDATOR:
+                    raise ValueError
+                
+            except ValueError:
+                print('What would you like to search for?')
+            
+            else:
+                break
+            
+        with open('Work_log.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                if search in row:
+                    print(row)
+                    
+    if choice == 'P':
+        file = open('Work_log.csv')
+        data = file.read()
+        file.close()
+        
+        while True:
+            try:
+                print('Please input a regular expression to search for')
+                reg_ex = input('What is the patter your looking for?: ')   
+                with open('Work_log.csv', 'r') as csvfile:
+                    reader = csv.DictReader(csvfile)
+                    for row in reader:
+                        search = re.findall(reg_ex, data)
+                        if search:
+                            print(row)
+                       
+                
+            except(ValueError, re.error):
+                print('please input a valid Regular Expression')
+            
+            else:
+                break
+       
+       
+                        
+    menu()
 
 
 def task(User):
@@ -153,7 +318,7 @@ def menu():
 if  __name__=='__main__':
     if os.path.isfile("Work_log.csv") != True:
         with open("Work_log.csv", "a") as csvfile:
-            fieldnames = ['Name', 'Task Name', 'Time Spent', 'Note']
+            fieldnames = ['Name', 'Task Name', 'Time Spent', 'Note', 'Date']
             notewriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
             notewriter.writeheader()
             
